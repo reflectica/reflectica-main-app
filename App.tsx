f227b8b177b59c1c5443378a03e763208ebf3a64
Chris Hong <hongchris97@gmail.com>
2024-07-17 01:30:01 -0700

    export via index in assets folder

--- App.tsx
+++ App.tsx
@@ -1,38 +1,182 @@
 import React from 'react';
+import {Image} from 'react-native';
 // import type {PropsWithChildren} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import { 
-  DashboardScreen, 
-  JournalScreen, 
-  SignupScreen, 
-  NotificationScreen, 
-  OnboardingScreen, 
-  PhonenumberScreen, 
-  ProfileScreen, 
-  SessionScreen, 
+import {
+  DashboardScreen,
+  JournalScreen,
+  SignupScreen,
+  NotificationScreen,
+  OnboardingScreen,
+  PhonenumberScreen,
+  ProfileScreen,
+  SessionScreen,
   SessionDetail,
-  VerificationScreen, 
-  LoginScreen, 
-  EmailLoginScreen, 
+  VerificationScreen,
+  LoginScreen,
+  EmailLoginScreen,
   SettingScreen,
   SupportScreen,
-  EmailSignupScreen
+  EmailSignupScreen,
 } from './src/screens';
-const Stack = createNativeStackNavigator();
+import {cloud, log, question, setting} from './src/assets/nav';
+import {useAuth} from './src/context/AuthContext';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+const JournalStack = createStackNavigator();
+
+function JournalStackNavigator() {
+  return (
+    <JournalStack.Navigator>
+      <JournalStack.Screen
+        name="Journal"
+        component={JournalScreen}
+        options={{headerShown: false}}
+      />
+      <JournalStack.Screen
+        name="SessionDetail"
+        component={SessionDetail}
+        options={{headerShown: false}}
+      />
+    </JournalStack.Navigator>
+  );
+}
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({route}) => ({
+        tabBarShowLabel: false,
+        tabBarIcon: ({color}) => {
+          let iconSource: any;
+
+          if (route.name === 'Dashboard') {
+            iconSource = cloud;
+          } else if (route.name === 'Journal') {
+            iconSource = log;
+          } else if (route.name === 'Support') {
+            iconSource = question;
+          } else if (route.name === 'Setting') {
+            iconSource = setting;
+          }
+          return (
+            <Image
+              source={iconSource}
+              style={{height: 30, width: 30}}
+              color={color}
+            />
+          );
+        },
+      })}>
+      <Tab.Screen
+        name="Dashboard"
+        component={DashboardScreen}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Journal"
+        component={JournalStackNavigator}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Support"
+        component={SupportScreen}
+        options={{headerShown: false}}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={SettingScreen}
+        options={{headerShown: false}}
+      />
+    </Tab.Navigator>
+  );
+}
 
 function App(): React.JSX.Element {
+  const {isLoggedIn, newUser} = useAuth();
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{title: 'Welcome'}}
-        />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        {isLoggedIn ? (
+          newUser ? (
+            <Stack.Screen
+              name="Profile"
+              component={ProfileScreen}
+              options={{headerShown: false}}
+            />
+          ) : (
+            <>
+              <Stack.Screen
+                name="MainApp"
+                component={MyTabs}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="In-Session"
+                component={SessionScreen}
+                options={{headerShown: false}}
+              />
+            </>
+          )
+        ) : (
+          <>
+            <Stack.Screen
+              name="In-Session"
+              component={SessionScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="MainApp"
+              component={MyTabs}
+              options={{headerShown: false}}
+            />
+
+            <Stack.Screen
+              name="Onboarding"
+              component={OnboardingScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Login"
+              component={LoginScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="EmailLogin"
+              component={EmailLoginScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Signup"
+              component={SignupScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="EmailSignup"
+              component={EmailSignupScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="PhoneNumber"
+              component={PhonenumberScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Verification"
+              component={VerificationScreen}
+              options={{headerShown: false}}
+            />
+            <Stack.Screen
+              name="Notification"
+              component={NotificationScreen}
+              options={{headerShown: false}}
+            />
+          </>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/assets/onboarding/index.ts
@@ -0,0 +1,5 @@
+import onboarding1 from './Onboarding1.png';
+import onboarding2 from './Onboarding2.png';
+import onboarding3 from './Onboarding3.png';
+
+export {onboarding1, onboarding2, onboarding3};
