6ac38bb1728cb7944b0edd65ae5c1ce65039515b
Chris Hong <hongchris97@gmail.com>
2024-07-23 00:12:43 -0700

    types for SessionDetailProp

--- src/constants/Session.ts
+++ src/constants/Session.ts
@@ -4,6 +4,28 @@ export interface SessionBoxesProp {
   description: string;
 }
 
+interface SessionScore {
+  score: number;
+  day: string;
+}
+
+interface EmotionalStateModeling {
+  happy: number;
+  excited: number;
+  nervous: number;
+  other: string;
+}
 export interface SessionDetailProp {
+  id: string;
   sessionID: number;
+  sessionScore: SessionScore[];
+  phq9: number[];
+  gad7: number[];
+  cbt: number[];
+  sfq: number[];
+  pss: number;
+  ssrs: number;
+  esteemBar: number[];
+  emotionalStateModeling: EmotionalStateModeling[];
+  keyConversationTopics: string[];
 }
